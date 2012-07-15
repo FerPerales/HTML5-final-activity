81a46e38e0c22abe00c821fd0a384a00a533c34d
Fer Perales <fernando.perales@live.com.mx>
2012-07-15 16:58:13 -0500

    First commit

---
+++ actividades/actividad19/js/scriptImagenes.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function(){
+                $("#pinkFloyd_min").mouseover(function(){
+                    $("#pinkFloyd").fadeIn(100);                    
+
+                });
+                $("#pinkFloyd_min").mouseout(function(){
+                    $("#pinkFloyd").fadeOut(10);                    
+
+                });
+                $("#eidyllion_min").mouseover(function(){
+                    $("#eidyllion").fadeIn(100);
+                    
+                });
+                $("#eidyllion_min").mouseout(function(){
+                    $("#eidyllion").fadeOut(10);
+                    
+                });
+                $("#elPersonal_min").mouseover(function(){
+                    $("#elPersonal").fadeIn(100);
+                    
+                });
+                $("#elPersonal_min").mouseout(function(){
+                    $("#elPersonal").fadeOut(10);
+                    
+                });
+                $("#doors_min").mouseover(function(){
+                    $("#doors").fadeIn(100);
+                   
+                });
+                $("#doors_min").mouseout(function(){
+                    $("#doors").fadeOut(10);                    
+                });
+            });
